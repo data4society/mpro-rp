26581391dc30f547542f00993535305c28538ae1
bormisov <bormisov@yandex.ru>
2016-05-13 12:24:45 +0500

    Библиотека pymystem3 с исправлением: результат  морфологического анализа с весами

---
+++ src/analyzer/pymystem3_w/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata.
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'pymystem3'
+project = "Python MyStem 3"
+project_no_spaces = project.replace(' ', '-')
+version = '0.1.5'
+description = 'Python wrapper for the Yandex MyStem 3'
+authors = ['Denis Sukhonin']
+authors_string = ', '.join(authors)
+emails = ['d.sukhonin@gmail.com']
+license = 'MIT'
+copyright = '2013, 2014, 2015 ' + authors_string
+url = 'https://github.com/Digsolab/pymystem3'
