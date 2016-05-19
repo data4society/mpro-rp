a7fece83d6fc2cf665902af63229505e7c4be8a1
Behlbor <behlbor@gmail.com>
2016-05-19 12:05:40 +0300

    setup.sh update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 sudo apt-get -y install python3-dev libpq-dev mc pip3
 sudo pip3 install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0-cp34-cp34m-linux_x86_64.whl
