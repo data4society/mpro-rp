267232efe336f894bcd2adb225a31075dd88bbb6
Behlbor <behlbor@gmail.com>
2016-05-19 21:06:05 +0300

    another attempt

--- setup.py
+++ setup.py
@@ -5,11 +5,7 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 
 requires = [
-    'sqlalchemy',
-    'sqlalchemy_utils',
-    'psycopg2',
-    'alembic',
-    'requests',
+    'SQLAlchemy',
     ]
 
 setup(name='mpro-rp',
