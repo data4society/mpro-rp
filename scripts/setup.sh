bb14febe58f8a71f79baf16aaf867a49791bd35a
Behlbor <behlbor@gmail.com>
2016-05-24 17:22:04 +0300

    nosetest test

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,4 +3,5 @@ sudo apt-get update
 #sudo apt-get -y upgrade
 sudo apt-get -y install python3-dev libpq-dev mc python3-pip python3-all-dev python3-nose
 sudo pip3 install SQLAlchemy
+sudo pip3 install psycopg2
 sudo pip3 install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0-cp34-cp34m-linux_x86_64.whl
