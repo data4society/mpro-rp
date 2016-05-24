ff3e26eb994aa2ba032fb735e461148b968546da
Behlbor <behlbor@gmail.com>
2016-05-24 14:27:47 +0300

    nosetest test

--- mprorp/config/settings.py
+++ mprorp/config/settings.py
@@ -1,6 +1,6 @@
 # The database settings are left blank so to force the use of local_settings.py below
 maindb_connection = ''
-testdb_connection = ''
+testdb_connection = 'aaaaa'
 try:
     from mprorp.config.local_settings import *
 except ImportError:
