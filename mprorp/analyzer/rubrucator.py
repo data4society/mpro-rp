fb2bb9ae194879a865f23b11b2b964208ebd7f9e
bormisov <bormisov@gmail.com>
2016-05-27 21:12:23 +0500

    random initialization model weights

--- mprorp/analyzer/rubrucator.py
+++ mprorp/analyzer/rubrucator.py
@@ -15,6 +15,7 @@ def morpho_doc(doc_id):
     #print(text)
     mystem_analyzer.start()
     new_morpho = mystem_analyzer.analyze(text)
+    #print(new_morpho)
     db.put_morpho(doc_id, new_morpho)
     mystem_analyzer.close()
 
@@ -89,10 +90,10 @@ def idf_object_features_set(set_id):
     # save to db: idf, indexes and object_features
     db.put_training_set_params(set_id, idf,  doc_index, lemma_index, object_features)
 
-    print(idf)
-    print(doc_index)
-    print(lemma_index)
-    print(object_features)
+    # print(idf)
+    # print(doc_index)
+    # print(lemma_index)
+    # print(object_features)
 
 def sigmoid_array(x):
     for l in range(len(x)):
@@ -106,9 +107,12 @@ def learning_rubric_model(set_id, rubric_id):
 
     # get answers for rubric
     answers = db.get_answers(set_id, rubric_id)
+    #print(answers)
     # get object_features, lemma_index, doc_index
     doc_index, object_features = db.get_doc_index_object_features(set_id)
 
+    #print(doc_index)
+    #print(object_features)
     doc_number = len(doc_index)
     # if we know answers, we can select most important features (mif):
     # mif[k] = l:
@@ -124,9 +128,15 @@ def learning_rubric_model(set_id, rubric_id):
     for doc_id in doc_index:
         answers_array[doc_index[doc_id], 0] = answers[doc_id] * 2 - 1
 
+    for doc_id in doc_index:
+        print(db.get_doc(doc_id))
+        index = doc_index[doc_id]
+        print(answers_array[index,:])
+        print(object_features[index])
+
     x = tf.placeholder(tf.float32, shape=[None, features_number])
     y_ = tf.placeholder(tf.float32, shape=[None, 1])
-    W = tf.Variable(tf.ones([features_number, 1]))
+    W = tf.Variable(tf.truncated_normal([features_number, 1],stddev=0.1)) #tf.truncated_normal(shape, stddev=0.1)
     b = tf.Variable(0.00001)
 
     y = tf.matmul(x,W) + b
@@ -138,7 +148,7 @@ def learning_rubric_model(set_id, rubric_id):
     sess = tf.Session()
     sess.run(init)
 
-    for i in range(50):
+    for i in range(5000):
         sess.run(train_step, feed_dict={x: object_features, y_: answers_array})
         #my_W = W.eval(sess)
         #my_b = b.eval(sess)
@@ -210,27 +220,7 @@ def spot_doc_rubrics(doc_id, rubrics):
         probability = sigmoid(np.dot(features_array, models[rubric_id]['model']))
         answers[rubric_id] = {'result':round(probability), 'model_id':models[rubric_id]['model_id']}
     db.put_rubrics(doc_id, answers)
-    print(answers)
+    print(doc_id, answers)
 
 
 mystem_analyzer = Mystem(disambiguation=False)
-#mystem_analyzer.start()
-
-#set_id = "1dcc4dc5-a706-4e61-8b37-26b5fd554145"    # Свобода слова 10
-rubric_id = '693a9b39-cb8e-4525-9333-1dadcda7c34e' # Свобода собраний
-
-set_id = "7fc5e799-2fa4-40ce-82cd-2793b6889002" # Свобода слова 100
-
-for doc_id in db.get_set_docs(set_id):
-    print(doc_id)
-    morpho_doc(doc_id)
-    lemmas_freq_doc(doc_id)
-
-idf_object_features_set(set_id)
-
-
-learning_rubric_model(set_id, rubric_id)
-
-for doc_id in db.get_set_docs(set_id):
-    spot_doc_rubrics(doc_id, {rubric_id: None})
-
