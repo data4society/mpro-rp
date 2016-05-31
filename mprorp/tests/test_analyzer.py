389c6412a3b08258a2268d1ade26f973bfe0644e
bormisov <bormisov@gmail.com>
2016-05-31 18:05:10 +0500

    idf for training set and model tests

--- mprorp/tests/test_analyzer.py
+++ mprorp/tests/test_analyzer.py
@@ -28,74 +28,68 @@ class SimpleDBTest(unittest.TestCase):
         lemmas = db.get_lemmas(doc_id)
         self.assertEqual(lemmas['склад'], 1)
 
+    def test_training_set_idf(self):
+
+        set_id, rubric_id = fill_db()
+        rb.idf_object_features_set(set_id)
+        result = select(ObjectFeatures.doc_id, ObjectFeatures.set_id == set_id).fetchall()
+        self.assertEqual(len(result), 10)
+
+    def test_model(self):
+
+        set_id, rubric_id = fill_db()
+        rb.idf_object_features_set(set_id)
+        rb.learning_rubric_model(set_id, rubric_id)
+        model = db.get_model(rubric_id, set_id)
+        self.assertEqual(model['features_num'], 10)
+
     def test_rubricator(self):
-        # insert 10 simple documents in db and form training set
-        # learn model for 1 rubric
-        # spot rubric for documents from same set
-        dropall_and_create()
 
-        tr_set = []
+        set_id, rubrics_id = fill_db()
 
-        doc_1 = Document(stripped="Письмо Маши Васе")
-        insert(doc_1)
-        tr_set.append(doc_1.doc_id)
-
-        doc_2 = Document(stripped="Первый документ Пети")
-        insert(doc_2)
-        tr_set.append(doc_2.doc_id)
-
-        doc_3 = Document(stripped="Первое письмо Маши")
-        insert(doc_3)
-        tr_set.append(doc_3.doc_id)
-
-        doc_4 = Document(stripped="Второй документ Маши")
-        insert(doc_4)
-        tr_set.append(doc_4.doc_id)
-
-        doc_5 = Document(stripped="Первый документ Васи")
-        insert(doc_5)
-        tr_set.append(doc_5.doc_id)
-
-        doc_6 = Document(stripped="Второй документ Пети")
-        insert(doc_6)
-        tr_set.append(doc_6.doc_id)
-
-        doc_7 = Document(stripped="Первое письмо Пети")
-        insert(doc_7)
-        tr_set.append(doc_7.doc_id)
-
-        doc_8 = Document(stripped="Первое письмо Васи")
-        insert(doc_8)
-        tr_set.append(doc_8.doc_id)
-
-        doc_9 = Document(stripped="Первый документ Маши")
-        insert(doc_9)
-        tr_set.append(doc_9.doc_id)
-
-        doc_10 = Document(stripped="Письмо Маши Пете")
-        insert(doc_10)
-        tr_set.append(doc_10.doc_id)
-
-        new_rubrics = Rubric(name="Маша")
-        insert(new_rubrics)
-        rubrics_id = str(new_rubrics.rubric_id)
-
-        insert(DocumentRubric(doc_id=str(tr_set[0]), rubric_id=rubrics_id))
-        insert(DocumentRubric(doc_id=str(tr_set[2]), rubric_id=rubrics_id))
-        insert(DocumentRubric(doc_id=str(tr_set[3]), rubric_id=rubrics_id))
-        insert(DocumentRubric(doc_id=str(tr_set[8]), rubric_id=rubrics_id))
-        insert(DocumentRubric(doc_id=str(tr_set[9]), rubric_id=rubrics_id))
-
-        for doc_id in tr_set:
-            rb.morpho_doc(doc_id)
-            rb.lemmas_freq_doc(doc_id)
-
-        set_id = str(db.put_training_set(tr_set))
         rb.idf_object_features_set(set_id)
         rb.learning_rubric_model(set_id, rubrics_id)
 
-        for doc_id in tr_set:
+        for doc_id in db.get_set_docs(set_id):
             rb.spot_doc_rubrics(doc_id, {rubrics_id: None})
 
         result = rb.compare_answers(db.get_model(rubrics_id, set_id)["model_id"], set_id, rubrics_id)
         print(result)
+
+
+def fill_db():
+
+    docs = ["Письмо Маши Васе",
+            "Первый документ Пети",
+            "Первое письмо Маши",
+            "Второй документ Маши",
+            "Первый документ Васи",
+            "Второй документ Пети",
+            "Первое письмо Пети",
+            "Первое письмо Васи",
+            "Первый документ Маши",
+            "Письмо Маши Пете"]
+    answers = [1,0,1,0,0,0,1,1,0,1]
+
+    tr_set = []
+
+    for doc in docs:
+        doc_db = Document(stripped=doc)
+        insert(doc_db)
+        tr_set.append(doc_db.doc_id)
+
+    new_rubric = Rubric(name="Маша")
+    insert(new_rubric)
+    rubric_id = str(new_rubric.rubric_id)
+
+    for i in range(10):
+        if answers[i]:
+            insert(DocumentRubric(doc_id=str(tr_set[i]), rubric_id=rubric_id))
+
+    for doc_id in tr_set:
+        rb.morpho_doc(str(doc_id))
+        rb.lemmas_freq_doc(str(doc_id))
+
+    set_id = str(db.put_training_set(tr_set))
+
+    return set_id, rubric_id
