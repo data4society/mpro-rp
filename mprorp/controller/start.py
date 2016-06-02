17b47c4f5b8c9f14dc07ecc133ff83e2b3818edf
Behlbor <behlbor@gmail.com>
2016-06-03 02:43:48 +0300

    next_crowl_time param and code for it

--- mprorp/controller/start.py
+++ mprorp/controller/start.py
@@ -1,5 +1,16 @@
 """main entry point"""
-class start:
-# test
-    if __name__ == "__main__":
-        pass;
+from mprorp.db.dbDriver import *
+from mprorp.db.models import *
+import datetime
+from mprorp.crawler.vk import vk_start_parsing
+
+
+
+def check_sources():
+    sources = select(Source.source_id, (Source.parse_period != -1) & (Source.next_crawling_time < datetime.datetime.now())).fetchall()
+    print(sources)
+    for source in sources:
+        vk_start_parsing(source[0])
+
+if __name__ == "__main__":
+    check_sources()
