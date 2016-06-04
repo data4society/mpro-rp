6b1b73e67c0c0fe2aff22f9da1d6a92c67a3fc45
bormisov <bormisov@gmail.com>
2016-06-05 00:14:44 +0500

    english words - count, numbers - ignore

--- mprorp/analyzer/test.py
+++ mprorp/analyzer/test.py
@@ -3,7 +3,7 @@ import mprorp.analyzer.rubricator as rb
 
 # set_id = u'293035dc-9db0-46b2-9247-f88423bbaef3'    # Свобода слова 100
 rubric_id = u'd2cf7a5f-f2a7-4e2b-9d3f-fc20ea6504da' # Свобода собраний
-
+rubric_id = u'6b11126a-5025-454a-bc27-f87f41cb1f09' # Политпрессинг
 # set_id = u'dc10f916-7f33-4d19-9ba9-0d2e6f24c947' # Свобода слова 10
 #
 set_id = u'cd63d6af-f0b2-4bbf-bea2-0b0e2294283d' # Все новости
@@ -16,8 +16,8 @@ set_id = u'cd63d6af-f0b2-4bbf-bea2-0b0e2294283d' # Все новости
 #    rb.lemmas_freq_doc(doc_id)
 # # #
 # rb.idf_object_features_set(set_id)
-# print('start learning')
-# rb.learning_rubric_model(set_id, rubric_id)
+print('start learning')
+rb.learning_rubric_model(set_id, rubric_id)
 
 for doc_id in db.get_set_docs(set_id):
     rb.spot_doc_rubrics(doc_id, {rubric_id: None})
