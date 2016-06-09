19a3ee92a7729536009e79a70b38ed598ef26f28
Behlbor <behlbor@gmail.com>
2016-06-09 18:09:48 +0300

    tomita and google init

--- mprorp/controller/start.py
+++ mprorp/controller/start.py
@@ -18,14 +18,15 @@ else:
 def check_sources():
     #vk_start_parsing.delay('2c00848d-dc19-4de0-a076-8d89c414a4fd')
     #return
-    sources = select(Source.source_id, (Source.parse_period != -1) &
+    sources = select([Source.source_id,Source.source_type_id], (Source.parse_period != -1) &
                      (Source.next_crawling_time < datetime.datetime.now()) &
                      (Source.wait == True)).fetchall()
     print("NEED CRAWL",sources)
     for source in sources:
         source_obj = Source(source_id=source[0], wait=False)
         update(source_obj)
-        vk_start_parsing.delay(source[0])
+        if source[1] == '': #  vk
+            vk_start_parsing.delay(source[0])
 
 
 
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,22 +3,22 @@
 sudo apt-get update
 #sudo apt-get -y upgrade
 # install some including pip3 and nosetests3
-sudo apt-get -y install python3-dev libpq-dev mc python3-pip python3-all-dev python3-nose
+sudo apt-get -y install python3-dev libpq-dev mc python3-pip python3-all-dev python3-nose python3-lxml
 # install project requirements
 sudo pip3 install -r requirements.txt
 # install tensorflow
 sudo pip3 install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0-cp34-cp34m-linux_x86_64.whl
 
-cd /home
-sudo mkdir tomita
+cd $HOME
+mkdir tomita
 cd tomita
 sudo apt-get -y install gcc-4.8 g++-4.8 cmake lua5.2 zip
-sudo wget https://github.com/yandex/tomita-parser/archive/master.zip
-sudo unzip master.zip
+wget https://github.com/yandex/tomita-parser/archive/master.zip
+unzip master.zip
 cd tomita-parser-master
 mkdir build
 cd build
-cmake ../src/ -DCMAKE_BUILD_TYPE=Release
+sudo cmake ../src/ -DCMAKE_BUILD_TYPE=Release
 sudo make
-sudo wget https://github.com/yandex/tomita-parser/releases/download/v1.0/libmystem_c_binding.so.linux_x64.zip
-sudo unzip libmystem_c_binding.so.linux_x64.zip
+wget https://github.com/yandex/tomita-parser/releases/download/v1.0/libmystem_c_binding.so.linux_x64.zip
+unzip libmystem_c_binding.so.linux_x64.zip
