7de7c40ec9222772b7517fcf01b2775330b1a9cb
matveykolbasov <matveykolbasov@yandex.ru>
2016-06-11 02:36:23 +0300

    Запуск Tomita

--- mprorp/tomita_grammars/test_tomita/tomita_run.py
+++ mprorp/tomita_grammars/test_tomita/tomita_run.py
@@ -1,6 +1,6 @@
 from mprorp.tomita_grammars.test_tomita.tomita_start import start_tomita
 from mprorp.tomita_grammars.test_tomita.tomita_out import tomita_out
 
-def run_tomita(grammar, file_name):
-    output = start_tomita(grammar, file_name)
+def run_tomita(grammar, doc_id):
+    output = start_tomita(grammar, doc_id)
     tomita_out(output)
