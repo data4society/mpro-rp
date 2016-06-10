dfe0d72c0888b6d460105a1760da25009d8c45ee
matveykolbasov <matveykolbasov@yandex.ru>
2016-06-10 20:38:23 +0300

    Запуск Tomita

--- mprorp/tomita_grammars/test_tomita/tomita_start.py
+++ mprorp/tomita_grammars/test_tomita/tomita_start.py
@@ -3,6 +3,7 @@ from os import chdir
 import os.path as path
 import subprocess as sp
 import re
+from mprorp.analyzer.db import get_doc
 
 grammars = {'person.cxx': 'bin',
             'date.cxx': 'bin2'}
@@ -84,13 +85,22 @@ TTextMinerConfig {
     config.write(config_file)
     config.close()
 
+def create_file(doc_id):
+    file_name = str(doc_id) + '.txt'
+    file = open(file_name, 'w', encoding='utf-8')
+    text = get_doc(doc_id)
+    file.write(text)
+    file.close()
+    return file_name
 
-def start_tomita(grammar, file_name):
+def start_tomita(grammar, doc_id):
     tomita_path_now = getcwd()
     home_path = re.findall('^(.*)\\\\', tomita_path_now)[0]
     tomita_path = path.join(home_path, grammars[grammar])
     grammar_name = re.findall('(.*)\\.cxx', grammar)[0]
     chdir(tomita_path)
+    # создаем файл с текстом
+    file_name = create_file(doc_id)
     # создаем config.proto
     create_config(grammar_name, file_name)
     # создаем dic.gzt
