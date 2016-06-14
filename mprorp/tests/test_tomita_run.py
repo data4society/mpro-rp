1dca2e3b651bdc6395d9eb58a4e69caf7c887c0c
matveykolbasov <matveykolbasov@yandex.ru>
2016-06-15 01:20:51 +0300

    Запуск Tomita

---
+++ mprorp/tests/test_tomita_run.py
@@ -0,0 +1,24 @@
+import unittest
+from mprorp.db.dbDriver import *
+from mprorp.tomita_grammars.test_tomita.tomita_run import run_tomita
+
+class SimpleTomitaTest(unittest.TestCase):
+
+    def test_tomita_person(self):
+        dropall_and_create()
+        key1 = '190:198'
+        key2 = '440:449'
+        key3 = '0:16'
+        value = 'Person'
+        dic_out = run_tomita('person.cxx', '000e82b8-6ea7-41f4-adc6-bc688fbbeeb6')
+        self.assertEqual(dic_out[key1], value)
+        self.assertEqual(dic_out[key2], value)
+        self.assertEqual(dic_out[key3], value)
+
+    def test_tomita_date(self):
+        dropall_and_create()
+        key = '38:47'
+        value = 'Date'
+        dic_out = run_tomita('date.cxx', '000e82b8-6ea7-41f4-adc6-bc688fbbeeb6')
+        self.assertEqual(dic_out[key], value)
+
