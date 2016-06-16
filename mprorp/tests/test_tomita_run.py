9ff50f5358aa6c616d1fa24e011c3198d4a69b74
matveykolbasov <matveykolbasov@yandex.ru>
2016-06-16 14:48:00 +0300

    Unittest для tomita

--- mprorp/tests/test_tomita_run.py
+++ mprorp/tests/test_tomita_run.py
@@ -1,24 +1,29 @@
 import unittest
 from mprorp.db.dbDriver import *
+from mprorp.db.models import *
 from mprorp.tomita_grammars.test_tomita.tomita_run import run_tomita
 
 class SimpleTomitaTest(unittest.TestCase):
 
     def test_tomita_person(self):
         dropall_and_create()
-        key1 = '180:188'
-        key2 = '440:449'
+        my_doc = Document(stripped='Алексей Бочкарев был задержан вечером 8 августа на Манежной площади за плакат, который, по мнению сотрудников полиции, оскорблял Путина.', type='article')
+        insert(my_doc)
+        doc_id = str(my_doc.doc_id)
+        dic_out = run_tomita('person.cxx',doc_id)
+        key1 = '129:135'
         key3 = '0:16'
         value = 'Person'
-        dic_out = run_tomita('person.cxx', '000e82b8-6ea7-41f4-adc6-bc688fbbeeb6')
         self.assertEqual(dic_out[key1], value)
-        self.assertEqual(dic_out[key2], value)
         self.assertEqual(dic_out[key3], value)
 
     def test_tomita_date(self):
         dropall_and_create()
+        my_doc = Document(stripped='Алексей Бочкарев был задержан вечером 8 августа на Манежной площади за плакат, который, по мнению сотрудников полиции, оскорблял Путина.',type='article')
+        insert(my_doc)
+        doc_id = str(my_doc.doc_id)
+        dic_out = run_tomita('date.cxx',doc_id)
         key = '38:47'
         value = 'Date'
-        dic_out = run_tomita('date.cxx', '000e82b8-6ea7-41f4-adc6-bc688fbbeeb6')
         self.assertEqual(dic_out[key], value)
 
