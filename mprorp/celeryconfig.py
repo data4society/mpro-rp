9208b2cd03cd5562fc4e4ce72743d255a6645f91
behlbor <behlbor@gmail.com>
2016-06-21 13:13:02 +0300

    crawler core

--- mprorp/celeryconfig.py
+++ mprorp/celeryconfig.py
@@ -1,8 +1,7 @@
 from datetime import timedelta
 from celery.schedules import crontab
 CELERY_INCLUDE = ['mprorp.controller.start','mprorp.crawler.vk','mprorp.crawler.google_news',
-                  'mprorp.crawler.site_page','mprorp.analyzer.rubricator','mprorp.tomita.regular',
-                  'mprorp.ner.regular']
+                  'mprorp.crawler.site_page']
 CELERYBEAT_SCHEDULE = {
                           'add-every-10-seconds': {
                               'task': 'mprorp.controller.start.check_sources',
--- mprorp/controller/logic.py
+++ mprorp/controller/logic.py
@@ -7,68 +7,71 @@ from mprorp.ner.tomita_to_markup import convert_tomita_result_to_markup
 
 from mprorp.celery_app import app
 
+TO_MORPHO_STATUS = 100
+TO_LEMMAS_STATUS = 101
+TO_RUBRICATION_STATUS = 102
+TO_MORPHO_STATUS = 200
+TO_MORPHO_STATUS = 300
+FINISH_STATUS = 1000
 
 if "worker" in sys.argv:
     celery = True
 else:
     celery = False
 
+rubrics_for_regular = {u'76819d19-d3f7-43f1-bc7f-b10ec5a2e2cc': u'404f1c89-53bd-4313-842d-d4a417c88d67'}  # 404f1c89-53bd-4313-842d-d4a417c88d67
+grammars = ['date.cxx', 'person.cxx']
+facts = ['Person']
 
-def router_func(doc_id, status):
+def router(doc_id, status):
     print("route:", doc_id, status)
     if status == 0:
         pass
-    if status == 1:
+    if status == 100:  # to morpho
         doc_id = str(doc_id)
         regular_morpho.delay(doc_id)
-    if status == 2:
+    if status == 101:  # to lemmas
         regular_lemmas.delay(doc_id)
-    if status == 3:
+    if status == 102:  # to rubrication
         regular_rubrication.delay(doc_id)
-    if status == 4:
+    if status >= 200 and status <= 200+len(grammars):  # to tomita
+        regular_tomita(status-10, doc_id, status+1)
+    if status == len(grammars)+1:  # to ner
+        regular_entities(doc_id)
+    if status == 300:  # fin regular processes
         doc = Document(doc_id = doc_id, status = 1000)
         update(doc)
 
 
-status = {'morpho': 2, 'lemmas': 3, 'rubrics': 4}
-rubrics_for_regular = {u'76819d19-d3f7-43f1-bc7f-b10ec5a2e2cc': u'404f1c89-53bd-4313-842d-d4a417c88d67'}  # 404f1c89-53bd-4313-842d-d4a417c88d67
 
 
 @app.task
-def regular_morpho(doc_id):
-    rb.morpho_doc(doc_id, status['morpho'])
-    router_func(doc_id, 2)
+def regular_morpho(doc_id, new_status):
+    rb.morpho_doc(doc_id, new_status)
+    router(doc_id, new_status)
 
 
 # counting lemmas frequency for one document
 @app.task
-def regular_lemmas(doc_id):
-    rb.lemmas_freq_doc(doc_id, status['lemmas'])
-    router_func(doc_id, 3)
+def regular_lemmas(doc_id, new_status):
+    rb.lemmas_freq_doc(doc_id, new_status)
+    router(doc_id, new_status)
 
 
-# regular ribrication
+# regular rubrication
 @app.task
-def regular_rubrication(doc_id):
-    rb.spot_doc_rubrics(doc_id, rubrics_for_regular, status['rubrics'])
-    # router_func(doc_id, 4)
-    regular_tomita(0, doc_id)
-
-
-grammars = ['date.cxx', 'person.cxx']
-facts = ['Person']
-
-@app.task
-def regular_tomita(grammar_index, doc_id):
-    run_tomita(grammars[grammar_index], doc_id, 10 + grammar_index)
-    if grammar_index == 0:
-        regular_tomita.delay(1, doc_id)
-    if grammar_index == 1:
-        regular_entities.delay(doc_id)
+def regular_rubrication(doc_id, new_status):
+    rb.spot_doc_rubrics(doc_id, rubrics_for_regular, new_status)
+    router(doc_id, new_status)
 
 
 @app.task
-def regular_entities(doc_id):
+def regular_tomita(grammar_index, doc_id, new_status):
+    run_tomita(grammars[grammar_index], doc_id, new_status)
+    router(doc_id, new_status)
+
+
+@app.task
+def regular_entities(doc_id, new_status):
     convert_tomita_result_to_markup(doc_id, ['person.cxx'])
-    doc = Document(doc_id=doc_id, status=1000)
-    update(doc)
+    router(doc_id, new_status)
