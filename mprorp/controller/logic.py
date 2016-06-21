3db97a68f484f391e11e70ad530845d6a473b1c0
Behlbor <behlbor@gmail.com>
2016-06-21 05:37:29 +0300

    crawler core

--- mprorp/controller/logic.py
+++ mprorp/controller/logic.py
@@ -1,7 +1,12 @@
 from mprorp.db.dbDriver import *
 from mprorp.db.models import *
 
-from mprorp.analyzer.rubricator import regular_morpho, regular_lemmas, regular_rubrication
+import mprorp.analyzer.rubricator as rb
+from mprorp.tomita.tomita_run import run_tomita
+from mprorp.ner.tomita_to_markup import convert_tomita_result_to_markup
+
+from mprorp.celery_app import app
+
 
 if "worker" in sys.argv:
     celery = True
@@ -23,3 +28,47 @@ def router_func(doc_id, status):
     if status == 4:
         doc = Document(doc_id = doc_id, status = 1000)
         update(doc)
+
+
+status = {'morpho': 2, 'lemmas': 3, 'rubrics': 4}
+rubrics_for_regular = {u'76819d19-d3f7-43f1-bc7f-b10ec5a2e2cc': u'404f1c89-53bd-4313-842d-d4a417c88d67'}  # 404f1c89-53bd-4313-842d-d4a417c88d67
+
+
+@app.task
+def regular_morpho(doc_id):
+    rb.morpho_doc(doc_id, status['morpho'])
+    router_func(doc_id, 2)
+
+
+# counting lemmas frequency for one document
+@app.task
+def regular_lemmas(doc_id):
+    rb.lemmas_freq_doc(doc_id, status['lemmas'])
+    router_func(doc_id, 3)
+
+
+# regular ribrication
+@app.task
+def regular_rubrication(doc_id):
+    rb.spot_doc_rubrics(doc_id, rubrics_for_regular, status['rubrics'])
+    # router_func(doc_id, 4)
+    regular_tomita(0, doc_id)
+
+
+grammars = ['date.cxx', 'person.cxx']
+facts = ['Person']
+
+@app.task
+def regular_tomita(grammar_index, doc_id):
+    run_tomita(grammars[grammar_index], doc_id, 10 + grammar_index)
+    if grammar_index == 0:
+        regular_tomita.delay(1, doc_id)
+    if grammar_index == 1:
+        regular_entities.delay(doc_id)
+
+
+@app.task
+def regular_entities(doc_id):
+    convert_tomita_result_to_markup(doc_id, ['person.cxx'])
+    doc = Document(doc_id=doc_id, status=1000)
+    update(doc)
