4a89f30ac2ddb506409f841836e72916ebdf9dab
Behlbor <behlbor@gmail.com>
2016-06-23 02:37:31 +0300

    readability test init

---
+++ mprorp/crawler/readability/test.py
@@ -0,0 +1,9 @@
+"""script for testing algorithms"""
+import mprorp.crawler.readability.readability_com as read_com
+
+
+
+
+
+if __name__ == '__main__':
+    print(read_com.find_full_text('http://www.moscow-post.com/news/society/vitse-premjer_karelii_popal_pod_podozrenie94812/'))
