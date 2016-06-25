9c6084ee6029a98e5e9e41931fe55a87f06c0389
matveykolbasov <matveykolbasov@yandex.ru>
2016-06-25 17:12:43 +0300

    Адрес

--- mprorp/tests/test_tomita_run.py
+++ mprorp/tests/test_tomita_run.py
@@ -31,11 +31,12 @@ class SimpleTomitaTest(unittest.TestCase):
 
     def test_tomita_loc(self):
         dropall_and_create()
-        my_doc = Document(stripped='Алексей Бочкарев был задержан вечером 8 августа на Манежной площади за плакат, который, по мнению сотрудников полиции, оскорблял Путина.',type='article')
+        my_doc = Document(stripped='В Одинцовском районе Московской области правоохранители задержали троих квартирных воров.',type='article')
         insert(my_doc)
         doc_id = str(my_doc.doc_id)
         dic_out = run_tomita('loc.cxx', doc_id)
-        key = '51:67'
+        print(dic_out)
+        key = '2:20'
         value = 'Loc'
         self.assertEqual(dic_out[key], value)
 
