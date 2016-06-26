064e833bf213a764e91e9bd3c5d311f22a966891
matveykolbasov <matveykolbasov@yandex.ru>
2016-06-27 01:26:27 +0300

    Персоны не выделяют названия улиц и пр

--- mprorp/tomita/grammars/person.cxx
+++ mprorp/tomita/grammars/person.cxx
@@ -1,5 +1,14 @@
 #encoding "utf-8"
 
-Pers1 -> Word<kwtype="FIO">;
+Pers -> Word<kwtype="FIO">;
 
-Person -> Pers1 interp(PersonFact_TOMITA.Person_TOMITA::not_norm);
+Wrong -> Word<kwset=~['street', 'location_front', 'location_back', "FIO"]>;
+
+Pers0 -> Pers interp(PersonFact_TOMITA.Person_TOMITA::not_norm) Wrong;
+Pers1 -> Pers<fw> interp(PersonFact_TOMITA.Person_TOMITA::not_norm) Punct;
+Pers2 -> Pers<fw> interp(PersonFact_TOMITA.Person_TOMITA::not_norm) Wrong;
+Pers3 -> Wrong Pers interp(PersonFact_TOMITA.Person_TOMITA::not_norm) Punct;
+Pers4 -> Wrong Pers interp(PersonFact_TOMITA.Person_TOMITA::not_norm) Wrong;
+Pers5 -> Pers2 Pers0* | Pers4 Pers0*;
+
+Person -> Pers1 | Pers2 | Pers3 | Pers4 | Pers5;
