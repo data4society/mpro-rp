f70151116cf8e2b732db7d62ea13f1c74e5f1faa
behlbor <behlbor@gmail.com>
2016-06-27 14:19:49 +0300

    readability2

--- mprorp/crawler/readability/breadability.py
+++ mprorp/crawler/readability/breadability.py
@@ -4,18 +4,10 @@ import urllib.request
 from mprorp.crawler.readability.libs.breadability.readable import Article
 
 
-def find_full_text(url):
-    try:
-        html_source = urllib.request.urlopen(url).read()  #
-        # html_source = send_get_request(url, gen_useragent=True)
-        # print(html)
-        doc = Article(html_source)
-        content = doc.readable
-        # title = doc.short_title()
+def find_full_text(html_source):
+    doc = Article(html_source)
+    content = doc.readable
 
-        stripped = strip_tags(content)
-        stripped = to_plain_text(stripped)
-        return stripped
-    except BaseException:
-        print('ERROR with ' + url)
-        return 'ERROR'
+    stripped = strip_tags(content)
+    stripped = to_plain_text(stripped)
+    return stripped
--- mprorp/crawler/readability/test.py
+++ mprorp/crawler/readability/test.py
@@ -7,6 +7,8 @@ import mprorp.crawler.readability.readability_kingwkb as kingwkb
 import csv
 from mprorp.crawler.readability.shingling import get_compare_estimate
 from mprorp.crawler.utils import *
+import urllib.request
+from urllib.error import *
 
 
 cloudflare_links = ['http://podrobnosti.ua/2115445-frantsuzy-edut-rassledovat-zaderzhanie-sbu-terrorista.html', 'http://ryb.ru/2016/06/21/348972',
@@ -18,16 +20,35 @@ cloudflare_links = ['http://podrobnosti.ua/2115445-frantsuzy-edut-rassledovat-za
     'http://u-f.ru/News/politics/u9/2016/06/18/101730',
     'http://newsday24.org/11294-savchenko-ustala-ot-takoy-zhizni.html']
 
+bad_links = ['http://pskov.riasv.ru/news/vo_frantsii_arestovali_krupnogo_pskovskogo_zemlevl/1110912/', 'http://podrobnosti.ua/2115445-frantsuzy-edut-rassledovat-zaderzhanie-sbu-terrorista.html', 'http://poliksal.ru/novosti-v-mire/45953-savchenko-prizvala-zapad-ostanovit-rossiyu-na-ukrainskoy-granice.html', 'http://ryb.ru/2016/06/21/348972', 'http://ryb.ru/2016/06/21/348890', 'http://ryb.ru/2016/06/21/349088', 'http://poliksal.ru/novosti-rosii/45899-novye-shokiruyuschie-zayavleniya-savchenko-ona-rasskazala-miru-o-planah-kremlya-zavoevat-evropu.html', 'http://poliksal.ru/novosti-v-mire/45894-v-strasburge-savchenko-prizvala-evropu-dressirovat-rossiyu.html', 'http://www.aysor.am/ru/news/2016/06/21/%D1%81%D0%B5%D1%84%D0%B8%D0%BB%D1%8F%D0%BD/1100847', 'http://news.am/rus/news/333253.html', 'http://news.am/rus/news/333232.html', 'http://news.am/rus/news/333247.html', 'http://u-f.ru/News/politics/u9/2016/06/18/101730', 'http://uinp.info/political_Groups/savchenko_v_strasburge_rezerv_rf_ischerpaetsya_togda_kogda_lyudi_perestanut_quotumirat_za_caryaquot', 'http://newsday24.org/11294-savchenko-ustala-ot-takoy-zhizni.html']
+
 
 def create_table(func, out_path):
     with open(out_path, 'w') as csvnewfile:
         spamwriter = csv.writer(csvnewfile)
         with open('corpus/readability_corpus.csv', 'r') as csvfile:
             spamreader = csv.reader(csvfile)
+            ind = 1
             for row in spamreader:
-                row.append(func(row[0]))
-                print(row[0])
-                spamwriter.writerow(row)
+                if not row[0] in bad_links:
+                    with open('corpus/docs/' + str(ind) + '.html', 'rb') as f:
+                        text = f.read()
+                        text = func(text)
+                        row.append(text)
+                        spamwriter.writerow(row)
+                ind += 1
+
+
+def create_table_remote(func, out_path):
+    with open(out_path, 'w') as csvnewfile:
+        spamwriter = csv.writer(csvnewfile)
+        with open('corpus/readability_corpus.csv', 'r') as csvfile:
+            spamreader = csv.reader(csvfile)
+            for row in spamreader:
+                if not row[0] in bad_links:
+                    row.append(func(row[0]))
+                    print(row[0])
+                    spamwriter.writerow(row)
 
 
 def create_estimates_table(in_path, out_path):
@@ -50,7 +71,8 @@ def get_final_estimate(in_path):
     with open(in_path, 'r') as csvfile:
         spamreader = csv.reader(csvfile)
         for row in spamreader:
-            if not row[0] in cloudflare_links:
+            #if not row[0] in cloudflare_links:
+            if row[2] != 'ERROR':
                 estim = float(row[3])
                 # print(estim)
                 estimates.append(estim)
@@ -58,10 +80,12 @@ def get_final_estimate(in_path):
                     estimates1.append(estim)
                 else:
                     bads += 1
+                    print(row[0])
                 if estim >= 90:
                     estimates90 += 1
                 if estim == 100:
                     estimates100 += 1
+    print('Размер корпуса: ' + str(len(estimates)))
     print('Среднее: ' + str(float(sum(estimates))/len(estimates)))
     print('Среднее без плохих: ' + str(float(sum(estimates1))/len(estimates1)))
     print('Плохие: ' + str(bads*100/len(estimates)) + '%')
@@ -92,9 +116,37 @@ def estimate_estimate():
                 print(get_compare_estimate(source,res))
 
 
+def create_corpus():
+    with open('corpus/readability_corpus.csv', 'r') as csvfile:
+        spamreader = csv.reader(csvfile)
+        ind = 1
+        bads = []
+        for row in spamreader:
+            url = row[0]
+            #print(url)
+            try:
+                html_source = urllib.request.urlopen(url,timeout=10).read()
+                with open('corpus/docs/' + str(ind) + '.html', 'wb') as f:
+                    f.write(html_source)
+            except BaseException as err:
+                if type(err) == HTTPError:
+                    print(url, err.code)
+                else:
+                    print(url, type(err))
+                bads.append(url)
+                #if err is HTTPError:
+                    #print(url, err.code)
+            ind += 1
+        #print(bads)
+
+
 if __name__ == '__main__':
-    #print(kingwkb.find_full_text('http://echospb.ru/2016/06/22/v-sb-ukraini-soobschili-chto-v-gosudarstve-net-baz-po/'))
+    #url = 'http://www.rosbalt.ru/russia/2016/06/17/1524253.html'
+    #html_source = urllib.request.urlopen(url, timeout=10).read()
+    #print(read2.find_full_text(html_source))
     #exit()
+
+    #create_corpus()
     #estimate_estimate()
 
     create_table(read2.find_full_text, 'corpus/read2.csv')
@@ -117,7 +169,7 @@ if __name__ == '__main__':
     print('Алгоритм lxml-readability')
     get_final_estimate('corpus/readability_lxml_estimates.csv')
 
-    #create_table(read_com.find_full_text, 'corpus/readability_com.csv')
+    #create_table_remote(read_com.find_full_text, 'corpus/readability_com.csv')
     #create_estimates_table('corpus/readability_com.csv', 'corpus/readability_com_estimates.csv')
     print('Алгоритм readability.com')
     get_final_estimate('corpus/readability_com_estimates.csv')
