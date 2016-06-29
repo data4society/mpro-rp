18a872340b37cbc2f3443c2eefc32371fc1c0ee1
matveykolbasov <matveykolbasov@yandex.ru>
2016-06-29 18:17:05 +0300

    Конфигурации (Название грамматики : Факт)

--- mprorp/tomita/grammars/person.cxx
+++ mprorp/tomita/grammars/person.cxx
@@ -1,14 +1,14 @@
 #encoding "utf-8"
 
-Pers -> Word<kwtype="FIO">;
+Pers -> Word<kwtype="FIO"> interp(PersonFact_TOMITA.Person_TOMITA::not_norm);
 
 Wrong -> Word<kwset=~['street', 'location_front', 'location_back', "FIO"]>;
 
-Pers0 -> Pers interp(PersonFact_TOMITA.Person_TOMITA::not_norm) Wrong;
-Pers1 -> Pers<fw> interp(PersonFact_TOMITA.Person_TOMITA::not_norm) Punct;
-Pers2 -> Pers<fw> interp(PersonFact_TOMITA.Person_TOMITA::not_norm) Wrong;
-Pers3 -> Wrong Pers interp(PersonFact_TOMITA.Person_TOMITA::not_norm) Punct;
-Pers4 -> Wrong Pers interp(PersonFact_TOMITA.Person_TOMITA::not_norm) Wrong;
+Pers0 -> Pers  Wrong | Pers  Punct;
+Pers1 -> Pers<fw> Punct;
+Pers2 -> Pers<fw> Wrong;
+Pers3 -> Wrong Pers Punct;
+Pers4 -> Wrong Pers Wrong;
 Pers5 -> Pers2 Pers0* | Pers4 Pers0*;
 
 Person -> Pers1 | Pers2 | Pers3 | Pers4 | Pers5;
