d01bab41495733cf9ef747db094a207b11da6d57
behlbor <behlbor@gmail.com>
2016-06-30 04:50:54 +0300

    update setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 # install some including pip3 and nosetests3
 sudo apt-get -y install python3-dev libpq-dev mc python3-pip python3-all-dev python3-nose python3-lxml
 # install project requirements
-sudo pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 # install tensorflow
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0rc0-cp34-cp34m-linux_x86_64.whl
 sudo pip3 install --upgrade $TF_BINARY_URL
