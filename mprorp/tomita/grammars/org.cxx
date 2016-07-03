b49f2a4d2211159a5f37ec6fdccc881283de6550
matveykolbasov <matveykolbasov@yandex.ru>
2016-07-03 22:42:54 +0300

    Update org.cxx

--- mprorp/tomita/grammars/org.cxx
+++ mprorp/tomita/grammars/org.cxx
@@ -1,7 +1,17 @@
 #encoding "utf-8"
 
 Trial -> Word<kwtype='trial'>;
-OOO -> Word<kwtype='OOO'> Word<h-reg2> | Word<kwtype='OOO'> Word<quoted> | Word<kwtype='OOO'> Word<lat>;
 
-Org -> Trial | OOO;
+OOO -> Word<kwtype='OOO'> Word<h-reg2>+ | Word<kwtype='OOO'> Word<quoted> | Word<kwtype='OOO'> Word<lat>;
+
+International -> Word<kwtype='inter'>;
+
+Org1 -> AnyWord<wfl="[А-Я][А-Я][А-Я]+"> | AnyWord<wfl="[А-Я][а-я]+([А-Я][а-я]+)+">;
+
+Fond -> 'фонд' Word<gram='gen'>+ | 'фонд' Word<gram='gen'>+ SimConjAnd Word<gram='gen'>+;
+Dop1 -> Noun<gram=~'nom'> | Adj<gram=~'nom'>;
+Dop2 -> SimConjAnd Dop1;
+Fonds -> Fond | Fond Prep* Dop1+ | Fond Prep* Dop1 Dop2+;
+
+Org -> Trial | OOO | International | Org1 | Fonds;
 Organisation -> Org interp(OrgFact_TOMITA.Org_TOMITA::not_norm);
