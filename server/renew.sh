cb823cab0ba4824e4cba2e378e730a55d5cf9bdf
Behlbor <behlbor@gmail.com>
2016-07-05 23:18:43 +0300

    renew script update

--- server/renew.sh
+++ server/renew.sh
@@ -5,4 +5,5 @@ rm -r mpro-rp-dev
 rm dev.zip
 wget https://github.com/data4society/mpro-rp/archive/dev.zip
 unzip dev.zip
+cp /home/mprorp/mpro-rp-dev/mprorp/config/local_settings.sample.py /home/mprorp/mpro-rp-dev/mprorp/config/local_settings.py
 /etc/init.d/celeryd restart
