84cf10355b53fb1b0852495c2fe49fef906c6956
matveykolbasov <matveykolbasov@yandex.ru>
2016-07-06 09:52:43 +0300

    Update out

--- mprorp/tomita/tomita_out.py
+++ mprorp/tomita/tomita_out.py
@@ -20,16 +20,18 @@ def list_make(text, source_name):
         if 'TOMITA =' in text[n]:
             fact = re.findall('TOMITA = (.*)', text[n])[0]
             name_fact = re.findall('(.*)_TOMITA', text[n])[0]
+            print(fact)
             first_symbol = sourse.find(fact)
+            fact = re.sub(' ,', ',', fact)
             if first_symbol != -1:
                 last_symbol = first_symbol + len(fact)
                 symbols = str(first_symbol + len_of_line) + ':' + str(last_symbol + len_of_line)
             else:
+                print('change')
                 fact_new = re.sub(' ', '', fact)
                 first_symbol = sourse.find(fact_new)
                 if first_symbol == -1:
-                    fact = re.sub(' "', ' «', fact)
-                    fact = re.sub('" ', '» ', fact)
+                    fact = re.sub('"(\w*)"?', '«\\1»', fact)
                     first_symbol = sourse.find(fact)
                     last_symbol = first_symbol + len(fact)
                     symbols = str(first_symbol + len_of_line) + ':' + str(last_symbol + len_of_line)
