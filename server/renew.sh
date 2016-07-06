a25e9554caf1cf0d9fa06a66bd2d6be8f999f745
Behlbor <behlbor@gmail.com>
2016-07-06 13:43:08 +0300

    server update

--- server/renew.sh
+++ server/renew.sh
@@ -7,3 +7,4 @@ wget https://github.com/data4society/mpro-rp/archive/dev.zip
 unzip dev.zip
 cp /home/mprorp/mpro-rp-dev/mprorp/config/local_settings.sample.py /home/mprorp/mpro-rp-dev/mprorp/config/local_settings.py
 /etc/init.d/celeryd restart
+echo "Renew code and restart system complete!"
