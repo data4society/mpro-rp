ab2201eff13202d481ec0e7ed8bbe6eccfed6d31
bormisov <bormisov@yandex.ru>
2016-07-21 23:30:55 +0300

    comments - model

--- mprorp/config/local_settings.py
+++
@@ -1,3 +0,0 @@
-# The database settings are left blank so to force the use of local_settings.py below
-maindb_connection = 'postgresql://postgres_mpro:qap9edMUKWixt6gAjTCz@46.101.162.206/postgres_mpro'
-testdb_connection = 'postgresql://postgres:password@localhost/postgres'
