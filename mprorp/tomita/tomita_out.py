c2dbaea55da537dcfee3d918ba21cd560e220e16
matveykolbasov <matveykolbasov@yandex.ru>
2016-07-21 14:38:55 +0300

    UPD tomita_out

--- mprorp/tomita/tomita_out.py
+++ mprorp/tomita/tomita_out.py
@@ -1,4 +1,7 @@
 import re
+import sys
+from colorama import init, AnsiToWin32
+stream = AnsiToWin32(sys.stderr).stream
 
 
 def text_make(file_name):
@@ -16,43 +19,46 @@ def list_make(text, source_name):
     sourse = re.sub('\n', ' ', sourse)
     out = {}
     len_of_line = 0
+    #ERROR = 0
     for n in range(len(text)):
         if 'TOMITA =' in text[n]:
             fact = re.findall('TOMITA = (.*)', text[n])[0]
             name_fact = re.findall('(.*)_TOMITA', text[n])[0]
-            first_symbol = sourse.find(fact)
+            #print('fact detected: ' + fact)
             fact = re.sub(' ,', ',', fact)
+            fact = re.sub('«', '', fact)
+            fact = re.sub('»', '', fact)
+            fact = re.sub('"', '', fact)
+            #print('fact changed: ' + fact)
+            first_symbol = sourse.find(fact)
             if first_symbol != -1:
                 last_symbol = first_symbol + len(fact)
                 symbols = str(first_symbol + len_of_line) + ':' + str(last_symbol + len_of_line)
             else:
-                fact_new = re.sub(' ', '', fact)
-                first_symbol = sourse.find(fact_new)
-                if first_symbol == -1:
-                    fact = re.sub('"(\w+)', '«\\1', fact)
-                    fact = re.sub('(\w+)"', '\\1»', fact)
-                    fact = re.sub('«', '', fact)
-                    fact = re.sub('»', '', fact)
-                    first_symbol = sourse.find(fact)
-                    last_symbol = first_symbol + len(fact)
-                    symbols = str(first_symbol + len_of_line) + ':' + str(last_symbol + len_of_line)
-                else:
-                    fact = fact_new
-                    last_symbol = first_symbol + len(fact)
-                    symbols = str(first_symbol + len_of_line) + ':' + str(last_symbol + len_of_line)
+                #print('change')
+                fact = re.sub(' ', '', fact)
+                first_symbol = sourse.find(fact)
+                last_symbol = first_symbol + len(fact)
+                symbols = str(first_symbol + len_of_line) + ':' + str(last_symbol + len_of_line)
 
-            #print(fact)
-            #print(symbols)
-            #print(sourse[first_symbol:last_symbol])
-            #print(s[first_symbol + len_of_line:last_symbol + len_of_line])
-            # print(sourse)
+            #print(sourse[:20])
+            #print('the last version of fact: ' + fact)
+            #print('symbols: ' + symbols)
+            #print('string now in original text: + 'sourse[first_symbol:last_symbol])
+            #print('string in original text: ' + s[first_symbol + len_of_line:last_symbol + len_of_line])
+            #if fact != s[first_symbol + len_of_line:last_symbol + len_of_line]:
+                #print('ERROR', file=stream)
+                #ERROR += 1
+            #print('\n')
 
             out[symbols] = name_fact
             sourse = sourse[last_symbol:]
             len_of_line += last_symbol
-    return out
 
+    #print('Error count = ' + str(ERROR) + '\n')
+    return out
 
 def tomita_out(file_name, source_name):
     out = list_make(text_make(file_name), source_name)
     return out
+
