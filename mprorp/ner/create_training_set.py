b6f13aa6620bde65a52aa40e79893020b640031c
matveykolbasov <matveykolbasov@yandex.ru>
2016-07-26 21:22:39 +0300

    training_set

--- mprorp/ner/create_training_set.py
+++ mprorp/ner/create_training_set.py
@@ -1,9 +1,10 @@
 import mprorp.db.dbDriver as Driver
 from mprorp.db.models import *
+from mprorp.analyzer.db import put_training_set
 
 def create_training_set(rubric_id, session=None):
     if session is None:
-        session = Driver.DBSession()
+        session = Driver.db_session()
     new_docs = session.query(Document).filter_by(status=1200)
     ids = []
     for doc in new_docs:
@@ -18,7 +19,7 @@ def create_training_set(rubric_id, session=None):
 
 def add_rubric_to_doc(rubrics_dic, rubric_id, session=None):
     if session is None:
-        session = Driver.DBSession()
+        session = Driver.db_session()
     for document_id in rubrics_dic[rubric_id]:
         new_id = DocumentRubric(doc_id = str(document_id), rubric_id = str(rubric_id))
         session.add(new_id)
@@ -32,9 +33,11 @@ def write_training_set(rubric_id, session=None):
  for doc_id in create_training_set(rubric_id):
   training_set.append(str(doc_id))
  if session is None:
-  session = Driver.DBSession()
+  session = Driver.db_session()
  new_set = TrainingSet(doc_ids=set(training_set), doc_num=len(training_set))
  session.add(new_set)
  session.commit()
 
-write_training_set('19848dd0-436a-11e6-beb8-9e71128cae02')
+#write_training_set('19848dd0-436a-11e6-beb8-9e71128cae02')
+
+#put_training_set(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae02'))
