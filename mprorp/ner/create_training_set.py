a639e6a6f3b059a2ea373cde3bf5b50cabba9d3e
matveykolbasov <matveykolbasov@yandex.ru>
2016-08-05 17:57:53 +0300

    UPD rubr

--- mprorp/ner/create_training_set.py
+++ mprorp/ner/create_training_set.py
@@ -71,16 +71,16 @@ def test_model(set_id, rubric_id):
 
 
 #1 Создание обучающей выборки
-print(len(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae21')[0]), len(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae21')[1])) #свобода ассоциаций
-print(len(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae02')[0]), len(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae02')[1])) #свобода собраний
+#print(len(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae21')[0]), len(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae21')[1])) #свобода ассоциаций
+#print(len(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae02')[0]), len(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae02')[1])) #свобода собраний
 #2 Запись в таблицу DocumentRubrics
-add_rubric_to_doc(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae21')[0], '19848dd0-436a-11e6-beb8-9e71128cae21') #свобода ассоциаций
-add_rubric_to_doc(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae02')[0], '19848dd0-436a-11e6-beb8-9e71128cae02') #свобода собраний
-add_rubric_to_doc(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae21')[1], '19848dd0-436a-11e6-beb8-9e71128cae21') #свобода ассоциаций
-add_rubric_to_doc(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae02')[1], '19848dd0-436a-11e6-beb8-9e71128cae02') #свобода собраний
+#add_rubric_to_doc(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae21')[0], '19848dd0-436a-11e6-beb8-9e71128cae21') #свобода ассоциаций
+#add_rubric_to_doc(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae02')[0], '19848dd0-436a-11e6-beb8-9e71128cae02') #свобода собраний
+#add_rubric_to_doc(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae21')[1], '19848dd0-436a-11e6-beb8-9e71128cae21') #свобода ассоциаций
+#add_rubric_to_doc(create_training_set('19848dd0-436a-11e6-beb8-9e71128cae02')[1], '19848dd0-436a-11e6-beb8-9e71128cae02') #свобода собраний
 #3 Запись в таблицу TrainingSet
-write_training_set('19848dd0-436a-11e6-beb8-9e71128cae21') #свобода ассоциаций
-write_training_set('19848dd0-436a-11e6-beb8-9e71128cae02') #свобода собраний
+#write_training_set('19848dd0-436a-11e6-beb8-9e71128cae21') #свобода ассоциаций
+#write_training_set('19848dd0-436a-11e6-beb8-9e71128cae02') #свобода собраний
 #4 Обучение
 #teach_rubricator(training_set_id, '19848dd0-436a-11e6-beb8-9e71128cae21') #свобода ассоциаций
 #teach_rubricator(training_set_id, '19848dd0-436a-11e6-beb8-9e71128cae02') #свобода собраний
