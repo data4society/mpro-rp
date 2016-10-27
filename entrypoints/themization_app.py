9fc4381b4acb811155c73b71ff7c6ee95da8e932
Behlbor <behlbor@gmail.com>
2016-10-27 17:07:20 +0300

    idf compute fix2

--- entrypoints/themization_app.py
+++ entrypoints/themization_app.py
@@ -1,11 +1,12 @@
 """start script for mass themization"""
 import sys
 sys.path.insert(0, '..')
-from mprorp.analyzer.theming.themer import mass_themization
+from mprorp.analyzer.theming.themer import mass_themization, compute_idfs
 from mprorp.analyzer.theming.test import get_estimate
 
 if __name__ == '__main__':
     print("STARTING MASS THEMIZATION")
-    mass_themization()
-    get_estimate()
+    compute_idfs()
+    #mass_themization()
+    #get_estimate()
     print("MASS THEMIZATION COMPLETE")
