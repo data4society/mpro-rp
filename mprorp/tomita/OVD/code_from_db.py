7dd2435354215491a7e20fb9abf4fbf240120278
matveykolbasov <matveykolbasov@yandex.ru>
2016-11-09 00:55:13 +0300

    OVD

--- mprorp/tomita/OVD/code_from_db.py
+++ mprorp/tomita/OVD/code_from_db.py
@@ -16,12 +16,11 @@ def get_level_of_fact(el):
     levels = el.split('_')
     return levels
 
-def get_codes_for_fact(fact):
+def get_codes_for_fact(fact, session):
     out = []
     fact_type_1 = ['Location']
     fact_type_2 = ['OVD', 'Name', 'Numb']
     fact_type_3 = ['City']
-    session = db_session()
     norms = fact['norm']
     for fact_type in norms:
         if fact_type in fact_type_1:
@@ -33,26 +32,29 @@ def get_codes_for_fact(fact):
                 for el in norms[fact_type]:
                     codes += session.query(KLADR).filter(KLADR.name_lemmas.has_key(el)).all()
                 out.append(codes)
-        #elif fact_type in fact_type_2:
+        elif fact_type in fact_type_2:
+            out.append([])
             #функция для таблицы с ОВД
         elif fact_type in fact_type_3:
-            all_codes = session.query(KLADR).filter(KLADR.name_lemmas.has_key(norms[fact_type][0])).all()
-            cities = session.query(KLADR).filter_by(fact_type='Город').all()
-            codes = cross(all_codes, cities)
+            codes = session.query(KLADR).filter(KLADR.name_lemmas.has_key(norms[fact_type][0]), KLADR.fact_type == 'Город').all()
             out.append(codes)
         else:
+            codes = []
             levels = get_level_of_fact(fact_type)
-            all_levels = []
-            for level in levels:
-                all_levels += session.query(KLADR).filter_by(level=level).all()
-            all_els = []
-            for el in norms[fact_type]:
-                all_els += session.query(KLADR).filter(KLADR.name_lemmas.has_key(el)).all()
-            codes = cross(all_levels, all_els)
+            for l in levels:
+                for el in norms[fact_type]:
+                    codes += session.query(KLADR).filter(KLADR.level == int(l), KLADR.name_lemmas.has_key(el)).all()
             out.append(codes)
     return out
 
 
 def get_all_codes(tomita_out_file, original_text):
+    session = db_session()
     all_facts = delete_loc(get_coordinates(tomita_out_file, original_text))
+    for fact in all_facts:
+        fact['codes'] = get_codes_for_fact(fact, session)
+    return all_facts
 
+a = get_all_codes('facts.xml', 'text.txt')
+for i in a:
+    print([len(ii) for ii in i['codes']])
--- mprorp/tomita/See_text.py
+++ mprorp/tomita/See_text.py
@@ -2,7 +2,7 @@ from mprorp.utils import home_dir
 import os
 from mprorp.db.dbDriver import *
 from mprorp.db.models import *
-from mprorp.tomita.OVD.code_from_db import *
+#from mprorp.tomita.OVD.code_from_db import *
 
 #text = get_doc('f844e3f0-4b02-4643-9ee3-2bdcd7c422be')
 #print(text)
@@ -13,6 +13,9 @@ from mprorp.tomita.OVD.code_from_db import *
 #path = os.path.dirname(os.path.realpath(__file__))
 #os.chdir(path + '/grammars')
 #session = db_session()
+#all_levels = []
+#all_levels += session.query(KLADR).filter_by(level=2).all()
+#print(a)
 #type1 = session.query(KLADR.kladr_id).filter_by(name_lemmas='{"строитель": 1}').all()
 #type2 = session.query(KLADR).filter(KLADR.name_lemmas.has_key("строитель")).all()
 #type3 = session.query(KLADR).filter_by(level=3).all()
