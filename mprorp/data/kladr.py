4f2f81e5183e4c263aae87c76ca165a1b62fc7bc
Behlbor <behlbor@gmail.com>
2016-11-14 17:06:28 +0300

    kladr import

--- mprorp/data/kladr.py
+++ mprorp/data/kladr.py
@@ -8,6 +8,11 @@ from sqlalchemy.orm import load_only
 from mprorp.analyzer.pymystem3_w import Mystem
 from sqlalchemy.orm.attributes import flag_modified
 
+import pymorphy2
+morph = pymorphy2.MorphAnalyzer()
+
+mystem = Mystem()
+mystem.start()
 socrs = dict()
 def import_kladr():
     with open(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) + '/kladr_data/SOCRBASE.csv',
@@ -100,7 +105,7 @@ def import_ovds():
 kladr_structure = {1: [0, 2], 2: [2, 5], 3: [5, 8], 4: [8, 11], 5: [11, 15]}
 def get_part_from_code(code, level):
     range = kladr_structure[level]
-    return int(code[range[0]:range[1]])
+    return code[range[0]:range[1]]
 
 
 def get_level(code):
@@ -112,15 +117,27 @@ def get_level(code):
         print(code, "ERROR")
         exit()
     for i in range(start_level, 0, -1):
-        if get_part_from_code(code, i):
+        if int(get_part_from_code(code, i)):
             return i
     print(code, "ERROR")
     exit()
 
 
+def get_parents_codes(code):
+    level = get_level(code)
+    codes = []
+    cur_code = ""
+    for i in range(1, level):
+        part = get_part_from_code(code, i)
+        cur_code += part
+        if int(part):
+            zeroes = '0' * (13-len(cur_code))
+            full_cur_code = cur_code + zeroes
+            codes.append(full_cur_code)
+    return codes
+
+
 def upd_kladr():
-    mystem = Mystem()
-    mystem.start()
     session = db_session()
     kladrs = session.query(KLADR).options(load_only("name")).all()
     n = 0
@@ -144,10 +161,195 @@ def upd_kladr():
             session.commit()
 
 
+def upd_ovds_tables():
+    with open(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) + '/kladr_data/ovds_out2.csv', 'w') as csvnewfile:
+        spamwriter = csv.writer(csvnewfile)
+        with open(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) + '/kladr_data/ovds_out.csv',
+                  'r') as csvfile:
+            spamreader = csv.reader(csvfile)
+            session = db_session()
+            n = 0
+            for row in spamreader:
+                #print(row[0])
+                #exit()
+                has_kladr = False
+                for i in range(5, 1, -1):
+                    if row[i]:
+                        has_kladr = True
+                        break
+                if not(has_kladr and ((i==5 and len(row[i]) == 17) or (i!=5 and len(row[i]) == 13))):
+                    print("KLADR ERROR", row)
+                    continue
+                kladr = row[i]
+                codes = get_parents_codes(kladr)
+                codes.append(kladr)
+                full_address = []
+                bad = False
+                for code in codes:
+                    #print(code)
+                    try:
+                        kladr_obj = session.query(KLADR).options(load_only("name","type")).filter_by(kladr_id=code).one()
+                        full_address.append(geoobject_name(kladr_obj.name, kladr_obj.type))
+                        if row[6]:
+                            full_address.append(row[6])
+                        full_address = ", ".join(full_address)
+                    except Exception as err:
+                        print("Error with code", code, "from row", row)
+                        full_address = "Error"
+                        bad = True
+                        break
+                #if bad:
+                #    continue
+                #print(full_address)
+                #print(row)
+                #print(kladr)
+                n += 1
+                print(n)
+                #continue
+                #row.append(entity.entity_id)
+                row.append(full_address)
+                spamwriter.writerow(row)
+
+
+def get_kladr_examples():
+    with open(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) + '/kladr_data/SOCRBASE.csv',
+              'r') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';')
+        data = False
+        for row in spamreader:
+            if data:
+                level = row[0]
+                if level not in socrs:
+                    socrs[level] = dict()
+                socrs[level][row[2]] = row[1]
+            else:
+                data = True
+
+    session = db_session()
+    #print(sorted(socrs.iteritems()))
+    #for level in socrs:
+    for level in sorted(socrs.keys()):
+        print("LEVEL:", level)
+        socrs_level = socrs[level]
+        #for socr in socrs_level:
+        for socr in sorted(socrs_level.keys()):
+            #print("    TYPE:", socr)
+            kladrs = session.query(KLADR).filter_by(type=socr).limit(10)
+            for kladr in kladrs:
+                #print("        " + kladr.name)
+                #if kladr.name == "Табачные":
+                print("        " + geoobject_name(kladr.name, kladr.type))
+                #break
+
+
+types_to_first = ["аал", "город", "поселение", "жилой район", "местечко", "микрорайон", "остров", "село", "садовое некоммерческое товарищество", "станица", "хутор", "гаражно-строительный кооператив", "дачное некоммерческое партнерство", "некоммерческое партнерство", "фермерское хозяйство", "ферма", "территория", "почтовое отделение", "городок", "железнодорожный пост", "железнодорожный разъезд", "жилая зона", "заимка", "населенный пункт", "погост", "животноводческая точка", "полустанок", "проселок", "строение", "участок"]
+mystem_to_pymorphy = {"nomn":"им","sing":"ед","plur":"мн","masc":"муж","femn":"жен","neut":"сред"}
+def geoobject_name(name, type):
+    if type == "Чувашия" and name == "Чувашская Республика -":
+        return "Чувашская Республика"
+    type = type.lower()
+    type = type.replace("муницип.образование","муниципальное образование")
+    if type in name.lower():
+        return name
+    name_first = True
+    for type0 in ["поселок", "платформа", "станция"]:
+        #print(type, type0)
+        if type0 in type:
+            name_first = False
+            #print("AAA", type)
+            break
+    if type in types_to_first:
+        #print("AAA", type)
+        name_first = False
+    if name_first:
+        name_morpho = mystem.analyze(name.lower())
+        #print(name_morpho)
+        name_is_a = False
+        for token in reversed(name_morpho):
+            if token["text"] in ["й", "я", "е"]:
+                name_is_a = True
+                a_number = "sing"
+                if token["text"] == "й":
+                    a_gender = "masc"
+                elif token["text"] == "я":
+                    a_gender = "femn"
+                else:
+                    a_gender = "neut"
+                break
+            if "analysis" in token:
+                for p in morph.parse(token["text"]):
+                    if p.tag.POS == "ADJF" and p.tag.case == "nomn" and p.score > 0.01:
+                        #print(p)
+                        name_is_a = True
+                        a_number = p.tag.number
+                        a_gender = p.tag.gender
+                        break
+                """
+                for variant in token["analysis"]:
+                    if "gr" in variant:
+                        gr = variant["gr"]
+                        #print(gr[0:2], gr)
+                        if gr[0:2] == "A=":
+                            name_is_a = True
+                            break
+                """
+                break
+        if name_is_a:
+            type_morpho = mystem.analyze(type)
+            #print(a_number, a_gender)
+            #print(type_morpho)
+            found = False
+            for token in type_morpho:
+                if "analysis" in token:
+                    to_mystem = False
+                    can_continue = False
+                    #print(morph.parse(token["text"]))
+                    for p in morph.parse(token["text"]):
+                        if p.score > 0.01:
+                            #print(p)
+                            #print(p.tag.POS)
+                            if p.tag.POS == "NOUN":
+                                if p.tag.case == "nomn" and p.tag.number == a_number and (a_gender == None or p.tag.gender == a_gender):
+                                    found = True
+                            elif p.tag.POS == None:
+                                to_mystem = True
+                                break
+                            elif p.tag.POS == "ADJF":
+                                if p.tag.case == "nomn" and p.tag.number == a_number and (a_gender == None or p.tag.gender == a_gender):
+                                    can_continue = True
+                    if to_mystem:
+                        for variant in token["analysis"]:
+                            if "gr" in variant:
+                                gr = variant["gr"]
+                                # print(gr[0:2], gr)
+                                if gr[0:1] == "S":
+                                    if mystem_to_pymorphy["nomn"] in gr and mystem_to_pymorphy[a_number] in gr and (a_gender == None or mystem_to_pymorphy[a_gender]) in gr:
+                                        found = True
+                                elif gr[0:1] == "A":
+                                    if mystem_to_pymorphy["nomn"] in gr and mystem_to_pymorphy[a_number] in gr and (a_gender == None or mystem_to_pymorphy[a_gender]) in gr:
+                                        can_continue = True
+                            else:
+                                name_first = False
+                    if not found:
+                        if can_continue:
+                            continue
+                        name_first = False
+                    break
+        else:
+            name_first = False
+    if name_first:
+        return name + " " + type
+    else:
+        return type + " " + name
+
 
 if __name__ == '__main__':
-    upd_kladr()
+    #print(mystem.analyze("ильгощинский"))
+    #print(morph.parse("ильгощинский обрыв"))
+    #upd_kladr()
     #import_kladr()
+    #get_kladr_examples()
+    upd_ovds_tables()
     #session = db_session()
     #ovds = session.query(Entity).filter(Entity.data.has_key("org_type")).count()
     #print(ovds)
