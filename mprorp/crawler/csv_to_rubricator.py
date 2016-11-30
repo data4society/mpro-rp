e8631e058c808b6ca09674afdd9b0d356a2e31a1
Behlbor <behlbor@gmail.com>
2016-11-30 03:02:05 +0300

    csv

--- mprorp/crawler/csv_to_rubricator.py
+++ mprorp/crawler/csv_to_rubricator.py
@@ -3,7 +3,7 @@
 from mprorp.db.models import *
 import csv
 from mprorp.utils import relative_file_path
-
+import datetime
 
 def csv_start_parsing(source, app_id, session):
     """download google news start feed and feeds for every story"""
@@ -14,10 +14,11 @@ def csv_start_parsing(source, app_id, session):
         for row in spamreader:
             rubric = session.query(Rubric).filter_by(name=row[0]).first()
             url = row[1]
+            date = datetime.datetime.fromtimestamp(datetime.datetime.now().timestamp())
             if session.query(Document).filter_by(guid=app_id + url).count() == 0:
                 meta = dict()
                 meta["publisher"] = dict()
-                doc = Document(guid=app_id + url, app_id=app_id, url=url, status=0, type='article', meta=meta, rubric_ids=[rubric.rubric_id])
+                doc = Document(guid=app_id + url, app_id=app_id, url=url, status=0, type='article', meta=meta, rubric_ids=[rubric.rubric_id], published_date=date)
                 session.add(doc)
                 docs.append(doc)
             else:
@@ -25,8 +26,8 @@ def csv_start_parsing(source, app_id, session):
                 rubric_ids = list(doc.rubric_ids)
                 rubric_ids.append(rubric.rubric_id)
                 doc.rubric_ids = rubric_ids
-            doc_rubric = DocumentRubric(doc=doc, rubric=rubric)
-            session.add(doc_rubric)
+            #doc_rubric = DocumentRubric(doc=doc, rubric=rubric)
+            #session.add(doc_rubric)
     return docs
 
 
