a30e8a95f24606dcaf5f6c29b161c073d04852b2
Behlbor <behlbor@gmail.com>
2016-12-02 16:41:18 +0300

    crawler logic fix

--- mprorp/controller/start.py
+++ mprorp/controller/start.py
@@ -7,7 +7,7 @@ from mprorp.db.models import *
 
 import datetime
 
-from mprorp.controller.logic import regular_gn_start_parsing, regular_ga_start_parsing, regular_vk_start_parsing, regular_yn_start_parsing, regular_csv_start_parsing, apps_config
+from mprorp.controller.logic import regular_gn_start_parsing, regular_ga_start_parsing, regular_vk_start_parsing, regular_yn_start_parsing, regular_csv_start_parsing
 from sqlalchemy.orm import load_only
 
 
