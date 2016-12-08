ea1497df0e4e5e7824ed5d426ced7955119beae1
MatveyKolbasov <matveykolbasov@yandex.ru>
2016-12-09 02:53:22 +0300

    Rajony UPD

--- mprorp/tomita/OVD/try.py
+++ mprorp/tomita/OVD/try.py
@@ -13,7 +13,7 @@ session = db_session()
 
 #!!!!СТРОКА СОДЕРЖИТ СТРОКУ a = session.query(Document).filter(Document.stripped.contains('отдел полиции №3')).all()
 
-a = session.query(Document).filter(Document.doc_id == '1e8df030-0aea-45c0-9407-d2385d4f535c').all()
+a = session.query(Document).filter(Document.doc_id == '0229dc23-91cd-4645-9035-63702652a4a3').all()
 #run_tomita2('ovd.cxx', '521d71f9-907e-4547-bfe1-77cc789e0220')
 #out = convert_tomita_result_to_markup(a[0], ['person.cxx'])
 #out = convert_tomita_result_to_markup(a[0], ['ovd.cxx'])
