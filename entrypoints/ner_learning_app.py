35311091d32b4c46629f178c5cdfe35d6578f6ce
Behlbor <behlbor@gmail.com>
2016-12-13 16:29:46 +0300

    ner learning fix

--- entrypoints/ner_learning_app.py
+++ entrypoints/ner_learning_app.py
@@ -2,12 +2,13 @@
 import sys
 sys.path.insert(0, '..')
 from mprorp.ner.NER import NER_person_learning, NER_learning_by_config
-from mprorp.controller.logic import apps_config
+from mprorp.db.dbDriver import *
 
 
 if __name__ == '__main__':
     print("STARTING NER LEARNING")
     #NER_person_learning()
+    apps_config = variable_get("last_config")
     for app_id in apps_config:
         app = apps_config[app_id]
         if "ner_predict" in app:
