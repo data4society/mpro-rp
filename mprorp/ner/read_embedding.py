d5915494abb02cafdfbdd4b9ea5d2de636430970
anton <bormisov@gmail.com>
2016-12-19 20:15:03 +0500

    sets for

--- mprorp/ner/read_embedding.py
+++ mprorp/ner/read_embedding.py
@@ -4,3 +4,4 @@ from gensim.models import word2vec
 
 pre_embedding_from_file = home_dir + '/embeddings/news_win20.model.bin'
 model_w2v = word2vec.Word2Vec.load_word2vec_format(pre_embedding_from_file, binary=True)
+print(model_w2v['жизнь_S'])
