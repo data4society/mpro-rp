6c11b4ff619a1fee398479c177438f2bd90a2e7c
Behlbor <behlbor@gmail.com>
2016-12-25 01:00:17 +0300

    rubrication upd

--- mprorp/controller/start.py
+++ mprorp/controller/start.py
@@ -32,9 +32,9 @@ def check_sources():
                             print(
                                 "CLEAR OLD: START DELETING DOCS WHERE APP=" + app_id + " AND TYPE=" + source_type + " AND SOURCE=" + source_key)
                             session.execute(
-                                "DELETE FROM mentions USING markups m, documents d WHERE m.markup_id = markup AND m.document = d.doc_id AND d.app_id = '" + app_id + "' AND d.source_with_type = '" + source_type+" "+source_key + "'")
+                                "DELETE FROM mentions r USING markups m, documents d WHERE m.markup_id = r.markup AND m.document = d.doc_id AND d.app_id = '" + app_id + "' AND d.source_with_type = '" + source_type+" "+source_key + "'")
                             session.execute(
-                                "DELETE FROM public.\"references\" USING markups m, documents d WHERE m.markup_id = markup AND m.document = d.doc_id AND d.app_id = '" + app_id + "' AND d.source_with_type = '" + source_type+" "+source_key + "'")
+                                "DELETE FROM public.\"references\" r USING markups m, documents d WHERE m.markup_id = r.markup AND m.document = d.doc_id AND d.app_id = '" + app_id + "' AND d.source_with_type = '" + source_type+" "+source_key + "'")
                             session.execute("DELETE FROM markups m USING documents d WHERE m.document = d.doc_id AND d.app_id = '" + app_id + "' AND d.source_with_type = '" + source_type+" "+source_key + "'")
                             session.execute("DELETE FROM rubricationresults r USING documents d WHERE r.doc_id = d.doc_id AND d.app_id = '" + app_id + "' AND d.source_with_type = '" + source_type+" "+source_key + "'")
                             session.execute("DELETE FROM documentrubrics r USING documents d WHERE r.doc_id = d.doc_id AND d.app_id = '" + app_id + "' AND d.source_with_type = '" + source_type+" "+source_key + "'")
