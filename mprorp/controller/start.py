adc4f9f3cf1170f11c7c0d3d1fb139f0fe78dcb6
Behlbor <behlbor@gmail.com>
2016-12-24 23:54:04 +0300

    rubrication upd

--- mprorp/controller/start.py
+++ mprorp/controller/start.py
@@ -31,6 +31,10 @@ def check_sources():
                         if "clear_old" in source:
                             print(
                                 "CLEAR OLD: START DELETING DOCS WHERE APP=" + app_id + " AND TYPE=" + source_type + " AND SOURCE=" + source_key)
+                            session.execute(
+                                "DELETE FROM mentions USING markups m, documents d WHERE m.markup_id = markup AND m.document = d.doc_id AND d.app_id = '" + app_id + "' AND d.source_with_type = '" + source_type+" "+source_key + "'")
+                            session.execute(
+                                "DELETE FROM public.\"references\" USING markups m, documents d WHERE m.markup_id = markup AND m.document = d.doc_id AND d.app_id = '" + app_id + "' AND d.source_with_type = '" + source_type+" "+source_key + "'")
                             session.execute("DELETE FROM markups m USING documents d WHERE m.document = d.doc_id AND d.app_id = '" + app_id + "' AND d.source_with_type = '" + source_type+" "+source_key + "'")
                             session.execute("DELETE FROM rubricationresults r USING documents d WHERE r.doc_id = d.doc_id AND d.app_id = '" + app_id + "' AND d.source_with_type = '" + source_type+" "+source_key + "'")
                             session.execute("DELETE FROM documentrubrics r USING documents d WHERE r.doc_id = d.doc_id AND d.app_id = '" + app_id + "' AND d.source_with_type = '" + source_type+" "+source_key + "'")
