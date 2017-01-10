0cbbf04fed8e01fbe20e8ee32b88de44cc6e2cba
Behlbor <behlbor@gmail.com>
2017-01-10 22:41:25 +0300

    timer init

---
+++ entrypoints/times.py
@@ -0,0 +1,11 @@
+"""get procedures times and write them"""
+import sys
+sys.path.insert(0, '..')
+
+if __name__ == '__main__':
+    print("START TIMING")
+    print(sys.argv[1])
+    print(type(sys.argv[1]))
+    #delete_document('f83eaba6-be71-4472-81f0-f57b8838461b')
+    #delete_app_documents('read_test')
+    print("FINISH TIMING")
