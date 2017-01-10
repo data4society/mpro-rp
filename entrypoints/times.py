fc4cbd19e3fcf50a545f065a911e70fc056e55f4
Behlbor <behlbor@gmail.com>
2017-01-10 23:55:24 +0300

    enviroment variables test

--- entrypoints/times.py
+++ entrypoints/times.py
@@ -1,10 +1,13 @@
 """get procedures times and write them"""
 import sys
 sys.path.insert(0, '..')
+import os
+
 
 if __name__ == '__main__':
     print("START TIMING")
-    print(sys.argv[1])
+    print(os.environ['TRAVIS_COMMIT_RANGE'])
+    print(os.environ['MY_PASSWORD'])
     print(type(sys.argv[1]))
     #delete_document('f83eaba6-be71-4472-81f0-f57b8838461b')
     #delete_app_documents('read_test')
