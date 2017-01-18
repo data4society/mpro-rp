1a83942eb92bc26abd35e5306cfaba4d7265a042
Behlbor <behlbor@gmail.com>
2017-01-18 04:53:58 +0300

    times on work server

--- mprorp/config/settings.py
+++ mprorp/config/settings.py
@@ -2,6 +2,11 @@
 # base settings config
 maindb_connection = ''
 testdb_connection = 'postgres://postgres:@localhost:5432/mprorp'  # for travis db
+google_private_key_id = ''
+google_private_key = ''
+google_client_email = ''
+google_client_id = ''
+google_spreadsheet_id = ''
 try:
     # trying override base settings with custom
     from mprorp.config.local_settings import *
