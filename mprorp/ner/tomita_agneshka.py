ada9bde3de7079dda4372a49c22557d079e1d53a
Behlbor <behlbor@gmail.com>
2017-01-20 20:04:48 +0300

    agneshka upd

--- mprorp/ner/tomita_agneshka.py
+++ mprorp/ner/tomita_agneshka.py
@@ -1,6 +1,13 @@
 from mprorp.tomita.grammars.config import config as grammar_config
 from mprorp.tomita.tomita_run import run_tomita2
 
+from mprorp.db.models import *
+from mprorp.db.dbDriver import *
+
+session = db_session()
+doc = session.query(Document).filter_by(doc_id='c8236b7e-b0a3-43c1-8601-cb57a055189d').first()
+print(repr(doc.stripped))
+
 for gram in grammar_config:
     run_tomita2(gram, 'c8236b7e-b0a3-43c1-8601-cb57a055189d')
 
