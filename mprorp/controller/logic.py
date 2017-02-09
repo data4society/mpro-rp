015fe6ce5071dee1f268a49775d7c6d7b2f4f531
Behlbor <behlbor@gmail.com>
2017-02-09 16:07:13 +0300

    logic times fix

--- mprorp/controller/logic.py
+++ mprorp/controller/logic.py
@@ -479,7 +479,7 @@ def regular_capital_feature(doc_id, new_status, **kwargs):
 
 
 #@app.task(ignore_result=True)
-@app.task()
+@app.task(time_limit=190, soft_time_limit=180)
 def regular_tomita(grammar, doc_id, new_status, **kwargs):
     """tomita"""
     session, doc = get_doc(doc_id, grammar=grammar)
@@ -542,7 +542,7 @@ def regular_theming(doc_id, new_status, **kwargs):
 
 
 #@app.task(ignore_result=True)
-@app.task(time_limit=190, soft_time_limit=180)
+@app.task()
 def tomita_entities(grammars_of_tomita_classes, doc_id, new_status, **kwargs):
     """
     grammars_of_tomita_classes = ['loc.cxx', 'org.cxx', 'norm_act.cxx']
