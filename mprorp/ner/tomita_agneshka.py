3268cb4ba1af4afce0c95c973b1e87421e23ab4b
MatveyKolbasov <matveykolbasov@yandex.ru>
2017-02-12 16:38:20 +0300

    Agneshka + time

--- mprorp/ner/tomita_agneshka.py
+++ mprorp/ner/tomita_agneshka.py
@@ -4,12 +4,15 @@ from mprorp.tomita.tomita_run import run_tomita2
 from mprorp.db.models import *
 from mprorp.db.dbDriver import *
 
+import time as Time
+
 session = db_session()
 doc = session.query(Document).filter_by(doc_id='c8236b7e-b0a3-43c1-8601-cb57a055189d').first()
 print(repr(doc.stripped))
 
 for gram in grammar_config:
-    print(gram)
+    a = Time.time()
     run_tomita2(gram, 'c8236b7e-b0a3-43c1-8601-cb57a055189d')
-    print('done ' + str(gram))
+    print(gram)
+    print('time: ' + str(Time.time() - a))
 
