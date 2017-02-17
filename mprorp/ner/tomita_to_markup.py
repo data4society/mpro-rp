cee298834904d136608256cb3e6f238f5bd48d47
bormisov <bormisov@yandex.ru>
2017-02-17 22:27:43 +0300

    tomita org/loc

--- mprorp/ner/tomita_to_markup.py
+++ mprorp/ner/tomita_to_markup.py
@@ -67,30 +67,30 @@ def convert_tomita_result_to_markup(doc, grammars,
             db.put_markup(doc, markup_name, classes.keys(), '20', refs, new_doc_markup=False, session=session,
                           commit_session=commit_session, verbose=verbose)
             # db.put_references(doc_id, markup_id, refs, new_status)
-        else:
-            default_entity = '0057375a-8242-1c6d-bf64-d5cb5a7ad7dd'
-            default_entities = {'location': '20b364e7-a5a9-4202-a8ef-4e5e987191fb',
-                                'org': '20b364e7-a5a9-4202-a8ef-4e5e987191fc'}
-            doc_id = doc.doc_id
-            results = db.get_tomita_results(doc_id, grammars)
-            print(results)
-            classes = {}
-            refs = []
-            for result in results:
-                # result - dict with keys like '15:22' and values like 'person' - tomita fact
-                for i in result:
-                    print(i)
-                    offsets = i.split(':')
-                    refs.append({
-                        'start_offset': int(offsets[0]), 'end_offset': int(offsets[1]),
-                        'len_offset': int(offsets[1]) - int(offsets[0]),
-                        'entity': default_entities.get(fact_entities[result[i]],
-                                                       default_entity) if entity is None else entity,
-                        'entity_class': fact_entities[result[i]]
-                    })
-                    classes[fact_entities[result[i]]] = ''
-            if verbose:
-                print('refs ', refs)
-            db.put_markup(doc, markup_name, classes.keys(), '20', refs, new_doc_markup=False, session=session,
-                          commit_session=commit_session, verbose=verbose)
+        # else:
+        #     default_entity = '0057375a-8242-1c6d-bf64-d5cb5a7ad7dd'
+        #     default_entities = {'location': '20b364e7-a5a9-4202-a8ef-4e5e987191fb',
+        #                         'org': '20b364e7-a5a9-4202-a8ef-4e5e987191fc'}
+        #     doc_id = doc.doc_id
+        #     results = db.get_tomita_results(doc_id, grammars)
+        #     print(results)
+        #     classes = {}
+        #     refs = []
+        #     for result in results:
+        #         # result - dict with keys like '15:22' and values like 'person' - tomita fact
+        #         for i in result:
+        #             print(i)
+        #             offsets = i.split(':')
+        #             refs.append({
+        #                 'start_offset': int(offsets[0]), 'end_offset': int(offsets[1]),
+        #                 'len_offset': int(offsets[1]) - int(offsets[0]),
+        #                 'entity': default_entities.get(fact_entities[result[i]],
+        #                                                default_entity) if entity is None else entity,
+        #                 'entity_class': fact_entities[result[i]]
+        #             })
+        #             classes[fact_entities[result[i]]] = ''
+        #     if verbose:
+        #         print('refs ', refs)
+        #     db.put_markup(doc, markup_name, classes.keys(), '20', refs, new_doc_markup=False, session=session,
+        #                   commit_session=commit_session, verbose=verbose)
             # db.put_references(doc_id, markup_id, refs, new_status)
