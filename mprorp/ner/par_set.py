a24bb4f754bdcc858eccdb1f35c82f31d123c255
anton <bormisov@gmail.com>
2017-04-18 17:45:30 +0500

    paragraph embed with regularization

--- mprorp/ner/par_set.py
+++ mprorp/ner/par_set.py
@@ -17,20 +17,33 @@ from six.moves.urllib.request import urlretrieve
 
 url = 'http://mattmahoney.net/dc/'
 consistent_words = True
-use_par_embed = True
-use_NN = False
-learning_rate = 0.1
+use_par_embed = False
+use_NN = True
+learning_rate = 0.3
 
 vocabulary_size = 50000
 paragraph_amount = 170000
 par_size = 100
 
 batch_size = 100
-embedding_size = 32 #128  # Dimension of the embedding vector.
-embed_par_size = 400
+embedding_size = 128 #128  # Dimension of the embedding vector.
+# embed_par_size = 400
 skip_window = 1  # How many words to consider left and right (if not consistent_words)
-num_skips = 2  # Size of the window with consistent or random oreder words
-l1_size = 32 #256
+num_skips = 2  # Size of the window with consistent or random order words
+l1_size = 128 #256
+
+reg_l1 = 0.0025
+reg_emded = 0.00005
+dropout = 0.7
+# reg_softmax = 0.00005
+
+# We pick a random validation set to sample nearest neighbors. here we limit the
+# validation samples to the words that have a low numeric ID, which by
+# construction are also the most frequent.
+valid_size = 25  # Random set of words to evaluate similarity on.
+valid_window = 300  # Only pick dev samples in the head of the distribution.
+valid_examples = np.array(random.sample(range(valid_window), valid_size))
+num_sampled = 64  # Number of negative examples to sample.
 
 def maybe_download(filename, expected_bytes):
   """Download a file if not present, and make sure it's the right size."""
@@ -92,14 +105,13 @@ par_index = 0
 def generate_batch(batch_size, num_skips, skip_window):
     global data_index
     global par_index
+    global use_par_embed
+
     assert batch_size % num_skips == 0
     assert num_skips <= 2 * skip_window
-    batch = np.ndarray(shape=(batch_size, num_skips), dtype=np.int32)
-    if not use_NN and use_par_embed:
-        labels = np.ndarray(shape=(batch_size), dtype=np.float32)
-    else:
-        labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
-    parags = np.ndarray(shape=(batch_size), dtype=np.int32)
+
+    batch = np.ndarray(shape=(batch_size, num_skips + use_par_embed), dtype=np.int32)
+    labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
     if consistent_words:
         span = num_skips + 1  # [ skip_window target skip_window ]
     else:
@@ -124,86 +136,61 @@ def generate_batch(batch_size, num_skips, skip_window):
                 batch[i, j] = buffer[target]
             else:
                 batch[i, j] = buffer[j]
+        if use_par_embed:
+            batch[i, num_skips] = par_index + vocabulary_size
 
-        if not use_NN and use_par_embed:
-            labels[i] = buffer[num_skips]
-        else:
-            labels[i, 0] = buffer[num_skips]
-        parags[i] = par_index
+        labels[i, 0] = buffer[num_skips]
         buffer.append(data[data_index + par_index * par_size])
         data_index = (data_index + 1) % par_size
         if data_index == 0:
             par_index = (par_index + 1) % paragraph_amount
         # buffer.append(data[data_index])
         # data_index = (data_index + 1) % len(data)
-    return batch, labels, parags
+    return batch, labels
 
 
 print('data:', [reverse_dictionary[di] for di in data[:8]])
 
 for num_skips, skip_window in [(2, 1), (4, 2)]:
     data_index = 0
-    batch, labels, parags = generate_batch(batch_size=8, num_skips=num_skips, skip_window=skip_window)
+    batch, labels = generate_batch(batch_size=8, num_skips=num_skips, skip_window=skip_window)
     print('\nwith num_skips = %d and skip_window = %d:' % (num_skips, skip_window))
-    print('    batch:', [reverse_dictionary[bii] for bi in batch for bii in bi])
+    print('    batch:', [reverse_dictionary[bii] if bii < vocabulary_size else bii for bi in batch for bii in bi])
     print('    labels:', [reverse_dictionary[li] for li in labels.reshape(8)])
-    print('    parags:', parags)
-
-
-# We pick a random validation set to sample nearest neighbors. here we limit the
-# validation samples to the words that have a low numeric ID, which by
-# construction are also the most frequent.
-valid_size = 16  # Random set of words to evaluate similarity on.
-valid_window = 100  # Only pick dev samples in the head of the distribution.
-valid_examples = np.array(random.sample(range(valid_window), valid_size))
-num_sampled = 64  # Number of negative examples to sample.
-
 
 graph = tf.Graph()
 
 with graph.as_default(), tf.device('/cpu:0'):
     # Input data.
-    train_dataset = tf.placeholder(tf.int32, shape=[batch_size, num_skips])
-    train_dataset_p = tf.placeholder(tf.int32, shape=[batch_size])
-    if not use_NN and use_par_embed:
-        train_labels = tf.placeholder(tf.int32, shape=[batch_size])
-    else:
-        train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+    features_number = num_skips + use_par_embed
+    train_dataset = tf.placeholder(tf.int32, shape=[batch_size, features_number])
+    train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
     valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
 
 
     # Variables.
+    input_vector_size = embedding_size * num_skips if not use_par_embed else embedding_size * (num_skips + 1)
+    embed_amount = vocabulary_size if not use_par_embed else vocabulary_size + paragraph_amount
+
+
     embeddings = tf.Variable(
-        tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
-    if use_par_embed:
-        embeddings_par = tf.Variable(
-            tf.random_uniform([paragraph_amount, embed_par_size], -1.0, 1.0))
+            tf.random_uniform([embed_amount, embedding_size], -1.0, 1.0))
 
     if use_NN:
         weights_l1 = tf.Variable(
-            tf.truncated_normal([embedding_size * num_skips, l1_size],
+            tf.truncated_normal([input_vector_size, l1_size],
                                 stddev=1.0 / math.sqrt(embedding_size)))
+        tf.add_to_collection('total_loss', 0.5 * reg_l1 * tf.nn.l2_loss(weights_l1))
 
         biases_l1 = tf.Variable(tf.zeros([l1_size]))
-
-        if use_par_embed:
-            weights_p = tf.Variable(
-                tf.truncated_normal([embed_par_size, l1_size],
-                                    stddev=1.0 / math.sqrt(embedding_size)))
-
         softmax_matrix_dim = l1_size
     else:
-        if use_par_embed:
-            weights_p = tf.Variable(
-                tf.truncated_normal([vocabulary_size, embed_par_size ],
-                                    stddev=1.0 / math.sqrt(embedding_size)))
-        softmax_matrix_dim = embedding_size * num_skips
-
-
+        softmax_matrix_dim = input_vector_size
 
     softmax_weights = tf.Variable(
         tf.truncated_normal([vocabulary_size, softmax_matrix_dim],
                             stddev=1.0 / math.sqrt(embedding_size)))
+    # tf.add_to_collection('total_loss', 0.5 * reg_softmax * tf.nn.l2_loss(softmax_weights))
 
     softmax_biases = tf.Variable(tf.zeros([vocabulary_size]))
 
@@ -211,43 +198,22 @@ with graph.as_default(), tf.device('/cpu:0'):
     # Look up embeddings for inputs.
     embed = tf.nn.embedding_lookup(embeddings, train_dataset)
     embed = tf.reshape(
-        embed, [-1, num_skips * embedding_size])
-    if use_par_embed:
-        embed_p = tf.nn.embedding_lookup(embeddings_par, train_dataset_p)
-
+        embed, [-1, input_vector_size])
+    tf.add_to_collection('total_loss', 0.5 * reg_emded * tf.nn.l2_loss(embed))
     # Compute the softmax loss, using a sample of the negative labels each time.
-    if use_NN and use_par_embed:
-        h = tf.nn.tanh(tf.matmul(embed, weights_l1) + tf.matmul(embed_p, weights_p) + biases_l1)
-        loss = tf.reduce_mean(
-            tf.nn.sampled_softmax_loss(weights=softmax_weights, biases=softmax_biases, inputs=h,
-                                       labels=train_labels, num_sampled=num_sampled, num_classes=vocabulary_size))
-    elif use_par_embed:
-        # print(embed)
-        # print(embed_p)
-        # h = tf.concat([1], [embed, embed_p])
-        # print(h)
-        #
-        # print(softmax_weights)
-        # print(weights_p)
-        # w = tf.concat([1], [softmax_weights, weights_p])
-        # print(w)
-        #
-        # loss = tf.reduce_mean(
-        #     tf.nn.sampled_softmax_loss(weights=w, biases=softmax_biases, inputs=h,
-        #                                labels=train_labels, num_sampled=num_sampled, num_classes=vocabulary_size))
-        loss = tf.reduce_mean(
-            tf.nn.sparse_softmax_cross_entropy_with_logits(logits=tf.matmul(embed, tf.transpose(softmax_weights)) +
-                                                    tf.matmul(embed_p, tf.transpose(weights_p)) + softmax_biases,
-                                                           labels=train_labels))
-    elif use_NN:
+    if use_NN:
         h = tf.nn.tanh(tf.matmul(embed, weights_l1) + biases_l1)
-        loss = tf.reduce_mean(
-            tf.nn.sampled_softmax_loss(weights=softmax_weights, biases=softmax_biases, inputs=h,
+        h_drop = tf.nn.dropout(h, dropout)
+        cross_entropy = tf.reduce_mean(
+            tf.nn.sampled_softmax_loss(weights=softmax_weights, biases=softmax_biases, inputs=h_drop,
                                        labels=train_labels, num_sampled=num_sampled, num_classes=vocabulary_size))
     else:
-        loss = tf.reduce_mean(
+        cross_entropy = tf.reduce_mean(
             tf.nn.sampled_softmax_loss(weights=softmax_weights, biases=softmax_biases, inputs=embed,
                                        labels=train_labels, num_sampled=num_sampled, num_classes=vocabulary_size))
+
+    tf.add_to_collection('total_loss', cross_entropy)
+    loss = tf.add_n(tf.get_collection('total_loss'))
 
     # Optimizer.
     # Note: The optimizer will optimize the softmax_weights AND the embeddings.
@@ -266,7 +232,7 @@ with graph.as_default(), tf.device('/cpu:0'):
     similarity = tf.matmul(valid_embeddings, tf.transpose(normalized_embeddings))
     init = tf.initialize_all_variables()
 
-num_steps = 100001
+num_steps = 200001
 
 with tf.Session(graph=graph) as session:
     # tf.global_variables_initializer().run()
@@ -274,17 +240,17 @@ with tf.Session(graph=graph) as session:
     print('Initialized')
     average_loss = 0
     for step in range(num_steps):
-        batch_data, batch_labels, batch_parags = generate_batch(
+        batch_data, batch_labels = generate_batch(
             batch_size, num_skips, skip_window)
-        feed_dict = {train_dataset : batch_data, train_dataset_p : batch_parags, train_labels : batch_labels}
+        feed_dict = {train_dataset : batch_data, train_labels : batch_labels}
         _, l = session.run([optimizer, loss], feed_dict=feed_dict)
         average_loss += l
         if step % 2000 == 0:
             if step > 0:
                 average_loss = average_loss / 2000
-        # The average loss is an estimate of the loss over the last 2000 batches.
-        print('Average loss at step %d: %f' % (step, average_loss))
-        average_loss = 0
+            # The average loss is an estimate of the loss over the last 2000 batches.
+            print('Average loss at step %d: %f' % (step, average_loss))
+            average_loss = 0
         # note that this is expensive (~20% slowdown if computed every 500 steps)
         if step % 10000 == 0:
             sim = similarity.eval()
@@ -294,7 +260,7 @@ with tf.Session(graph=graph) as session:
                 nearest = (-sim[i, :]).argsort()[1:top_k+1]
                 log = 'Nearest to %s:' % valid_word
                 for k in range(top_k):
-                    close_word = reverse_dictionary[nearest[k]]
+                    close_word = reverse_dictionary[nearest[k]] if nearest[k] < vocabulary_size else nearest[k] - vocabulary_size
                     log = '%s %s,' % (log, close_word)
                 print(log)
     final_embeddings = normalized_embeddings.eval()
