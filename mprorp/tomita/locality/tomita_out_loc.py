820f3bf6cc1f310a588f337c67bd4df0ced0c30d
MatveyKolbasov <matveykolbasov@yandex.ru>
2017-06-02 16:51:09 +0300

    Locality UPD

--- mprorp/tomita/locality/tomita_out_loc.py
+++ mprorp/tomita/locality/tomita_out_loc.py
@@ -26,13 +26,14 @@ def get_coordinates(facts, sourse, tomita_path):
     text = open(tomita_path + '/' + facts, 'r', encoding='utf-8').read()
     sourse = open(tomita_path + '/' + sourse, 'r', encoding='utf-8').read()
     facts = re.findall('<.*?_TOMITA FactID="(\d+?)" .*?pos="(\d+?)" len="(\d+?)" sn="(\d+?)".*?>(.*?)</([A-z]*?)_TOMITA>', text)
-    l = [{'id' : int(i[0]),
-          'fs' : int(i[1]),
-          'string' : sourse[int(i[1]):int(i[1])+int(i[2])].lower(),
-          'facts' : re.findall('<(.*?)_TOMITA val="(.*?)" pos', i[4]),
-          'type' : i[5],
-          'sn' : int(i[3]),
-          'ls': int(i[1]) + int(i[2])} for i in facts]
+    l = [{'id': int(i[0]),
+          'fs': int(i[1]),
+          'string': sourse[int(i[1]):int(i[1])+int(i[2])].lower(),
+          'facts': re.findall('<(.*?)_TOMITA val="(.*?)" pos', i[4]),
+          'type': i[5],
+          'sn': int(i[3]),
+          'ls': int(i[1]) + int(i[2])} for i in facts if sourse[int(i[1]) - 1] not in ['"', '«']
+         and sourse[int(i[1]) + int(i[2]) + 1] not in ['"', '»']]
     for n in range(len(l)):
         l[n] = coordinates(l[n])
         l[n] = normalization(l[n])
